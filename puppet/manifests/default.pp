bc5f6f395b0dc6558869815c3b599a298f5a4db7
Paul Hamby <paul.hamby@bancvue.com>
2013-07-06 11:29:53 -0500

    adding rvm module

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,3 +1,23 @@
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
+class system-update {
+
+  exec { 'apt-get update':
+    command => 'apt-get update',
+  }
+
+}
+
+class rvm-install {
+
+  include rvm
+
+  rvm_system_ruby {
+    'ruby-1.9.3-p429':
+      ensure => 'present',
+      default_use => true;
+  }
+}
+
+include system-update
 include mysql
