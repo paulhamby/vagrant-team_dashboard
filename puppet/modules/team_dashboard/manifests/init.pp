222934b164fc3e65ad2f1a4a1c8af41e3276e2da
Paul Hamby <paul.hamby@bancvue.com>
2013-07-06 16:49:49 -0500

    adding rvm to path

--- puppet/modules/team_dashboard/manifests/init.pp
+++ puppet/modules/team_dashboard/manifests/init.pp
@@ -23,7 +23,7 @@ class team_dashboard {
   }
 
   exec { 'install-team_dashboard':
-    #path => '/usr/bin:/usr/sbin:/bin:/usr/local/rvm/bin/rvm',
+    path => '/usr/bin:/usr/sbin:/bin:/usr/local/rvm/bin',
     command => "bash -c 'cd /vagrant/team_dashboard && bundle install && rake db:create && rake db:migrate && touch /vagrant/team_dashboard_installed'",
     creates => '/vagrant/team_dashboard_installed',
     require => [ File['/vagrant/team_dashboard/config/database.yml'] ],
