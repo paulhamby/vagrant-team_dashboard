cc9cd2a2af8ada37ad22ddc20e7391423b3ca1c5
Paul Hamby <paul.hamby@bancvue.com>
2013-07-06 15:36:42 -0500

    fleshing out team_dashboard module

--- puppet/modules/team_dashboard/manifests/init.pp
+++ puppet/modules/team_dashboard/manifests/init.pp
@@ -4,7 +4,7 @@ class team_dashboard {
     ensure => installed,
   }
 
-  vcsrepo { "/vagrant/team_dashboard/team_dashboard":
+  vcsrepo { "/vagrant/team_dashboard":
     ensure   => latest,
     owner    => vagrant,
     group    => vagrant,
@@ -12,5 +12,39 @@ class team_dashboard {
     require  => [ Package["git"] ],
     source   => "https://github.com/paulhamby/team_dashboard",
     revision => 'master',
+  }
+
+#cd team_dashboard
+#bundle install
+#cp config/database.example.yml config/database.yml
+#rake db:create && rake db:migrate
+
+  exec { 'install-team_dashboard':
+    #path => '/usr/bin:/usr/sbin:/bin:/usr/local/rvm/bin/rvm',
+    command => "bash -c 'cd /vagrant/team_dashboard && bundle install && rake db:create && rake db:migrate && touch /tmp/team_dashboard_installed'",
+    creates => '/vagrant/team_dashboard_installed',
+    require => [ Vcsrepo['/vagrant/team_dashboard'] ],
+  }
+
+  file { "/etc/unicorn":
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    require => [ Vcsrepo['/vagrant/team_dashboard'] ],
+  }
+
+  file { "/etc/unicorn/team_dashboard.conf":
+    owner  => root,
+    group  => root,
+    source => 'puppet:///modules/team_dashboard/team_dashboard.conf',
+    require => [ File['/etc/unicorn'] ],
+  }
+
+  file { "/etc/init.d/unicorn":
+    owner   => root,
+    group   => root,
+    mode    => 755,
+    source  => 'puppet:///modules/team_dashboard/unicorn.init',
+    require => [ File['/etc/unicorn'] ],
   }
 }
