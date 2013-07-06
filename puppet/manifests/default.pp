ab15ee823d5c4d75b401695d7c4630237767187e
Paul Hamby <paul.hamby@bancvue.com>
2013-07-06 15:43:45 -0500

    fleshing out team_dashboard module

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -6,6 +6,12 @@ class system-update {
     command => 'apt-get update',
   }
 
+  $sysPackages = [ "build-essential" ]
+  package { $sysPackages:
+    ensure => "installed",
+    require => Exec['apt-get update'],
+  }
+
 }
 
 class rvm-install {
